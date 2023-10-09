e655b29663db27ec39681d4e4db62d5246886aff
Ejiamike <ejiamikealbert@gmail.com>
2023-10-09 01:20:33 +0100

    I added two(2) buttons 1..Run button and 2..Syntax button, also I changed the names(i.e Text) of the buttons to be Run and Syntax which was previously (button1 button2)

--- GraphicalProgrammingLanguage/Form1.Designer.cs
+++ GraphicalProgrammingLanguage/Form1.Designer.cs
@@ -28,42 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            textBox1 = new TextBox();
             richTextBox1 = new RichTextBox();
-            panel1 = new Panel();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(12, 274);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(372, 23);
-            textBox1.TabIndex = 2;
             // 
             // richTextBox1
             // 
-            richTextBox1.Location = new Point(12, 42);
+            richTextBox1.Location = new Point(24, 12);
             richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(306, 205);
-            richTextBox1.TabIndex = 6;
+            richTextBox1.Size = new Size(257, 232);
+            richTextBox1.TabIndex = 0;
             richTextBox1.Text = "";
             // 
-            // panel1
+            // textBox1
             // 
-            panel1.BackColor = SystemColors.ActiveBorder;
-            panel1.Location = new Point(348, 42);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(339, 205);
-            panel1.TabIndex = 7;
+            textBox1.Location = new Point(24, 260);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(322, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(24, 298);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Run";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(127, 298);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 3;
+            button2.Text = "Syntax";
+            button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(738, 511);
-            Controls.Add(panel1);
-            Controls.Add(richTextBox1);
+            ClientSize = new Size(679, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(textBox1);
+            Controls.Add(richTextBox1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -71,8 +83,10 @@
         }
 
         #endregion
-        private TextBox textBox1;
+
         private RichTextBox richTextBox1;
-        private Panel panel1;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
     }
 }
