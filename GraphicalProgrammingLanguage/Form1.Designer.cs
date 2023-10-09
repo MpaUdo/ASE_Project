5d1fe4cfa7c803939a81335c12bb8b0bddee16c3
Ejiamike <ejiamikealbert@gmail.com>
2023-10-09 01:11:35 +0100

    I added a panel to display my command Output and changed the BackColor to grey

--- GraphicalProgrammingLanguage/Form1.Designer.cs
+++ GraphicalProgrammingLanguage/Form1.Designer.cs
@@ -30,13 +30,14 @@
         {
             textBox1 = new TextBox();
             richTextBox1 = new RichTextBox();
+            panel1 = new Panel();
             SuspendLayout();
             // 
             // textBox1
             // 
             textBox1.Location = new Point(12, 274);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(260, 23);
+            textBox1.Size = new Size(372, 23);
             textBox1.TabIndex = 2;
             // 
             // richTextBox1
@@ -47,11 +48,20 @@
             richTextBox1.TabIndex = 6;
             richTextBox1.Text = "";
             // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveBorder;
+            panel1.Location = new Point(348, 42);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(339, 205);
+            panel1.TabIndex = 7;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(738, 511);
+            Controls.Add(panel1);
             Controls.Add(richTextBox1);
             Controls.Add(textBox1);
             Name = "Form1";
@@ -63,5 +73,6 @@
         #endregion
         private TextBox textBox1;
         private RichTextBox richTextBox1;
+        private Panel panel1;
     }
 }
