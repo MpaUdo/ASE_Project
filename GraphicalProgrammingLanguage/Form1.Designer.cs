8af073eb1f3a2a0f6b10af1158fb5562016b000f
Ejiamike <ejiamikealbert@gmail.com>
2023-11-07 05:39:59 +0100

    more changes to the UI

--- GraphicalProgrammingLanguage/Form1.Designer.cs
+++ GraphicalProgrammingLanguage/Form1.Designer.cs
@@ -33,8 +33,9 @@
             btnRun = new Button();
             button2 = new Button();
             pictureBox1 = new PictureBox();
-            textBox2 = new TextBox();
+            textBoxCmdLine = new TextBox();
             pictureBox2 = new PictureBox();
+            button1 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
@@ -48,24 +49,27 @@
             // 
             // btnRun
             // 
-            btnRun.BackColor = Color.MediumOrchid;
+            btnRun.BackColor = SystemColors.ActiveCaption;
             btnRun.FlatStyle = FlatStyle.Popup;
             btnRun.Font = new Font("Times New Roman", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            btnRun.Location = new Point(24, 327);
+            btnRun.Location = new Point(38, 318);
             btnRun.Name = "btnRun";
-            btnRun.Size = new Size(75, 23);
+            btnRun.Size = new Size(65, 23);
             btnRun.TabIndex = 2;
             btnRun.Text = "Run";
             btnRun.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
-            button2.Location = new Point(124, 327);
+            button2.BackColor = SystemColors.ActiveCaption;
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.Font = new Font("Times New Roman", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(109, 318);
             button2.Name = "button2";
-            button2.Size = new Size(75, 23);
+            button2.Size = new Size(65, 23);
             button2.TabIndex = 3;
             button2.Text = "Syntax";
-            button2.UseVisualStyleBackColor = true;
+            button2.UseVisualStyleBackColor = false;
             // 
             // pictureBox1
             // 
@@ -74,18 +78,17 @@
             pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
             pictureBox1.Location = new Point(38, 356);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(555, 137);
+            pictureBox1.Size = new Size(571, 137);
             pictureBox1.TabIndex = 5;
             pictureBox1.TabStop = false;
             // 
-            // textBox2
+            // textBoxCmdLine
             // 
-            textBox2.BorderStyle = BorderStyle.FixedSingle;
-            textBox2.Location = new Point(24, 27);
-            textBox2.Multiline = true;
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(285, 244);
-            textBox2.TabIndex = 6;
+            textBoxCmdLine.Location = new Point(24, 27);
+            textBoxCmdLine.Multiline = true;
+            textBoxCmdLine.Name = "textBoxCmdLine";
+            textBoxCmdLine.Size = new Size(285, 244);
+            textBoxCmdLine.TabIndex = 6;
             // 
             // pictureBox2
             // 
@@ -96,17 +99,32 @@
             pictureBox2.TabIndex = 7;
             pictureBox2.TabStop = false;
             // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Times New Roman", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(544, 318);
+            button1.Name = "button1";
+            button1.Size = new Size(65, 23);
+            button1.TabIndex = 8;
+            button1.Text = "Clear";
+            button1.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ScrollBar;
             ClientSize = new Size(648, 505);
+            Controls.Add(button1);
             Controls.Add(pictureBox2);
-            Controls.Add(textBox2);
+            Controls.Add(textBoxCmdLine);
             Controls.Add(pictureBox1);
             Controls.Add(button2);
             Controls.Add(btnRun);
             Controls.Add(textBox1);
+            MaximizeBox = false;
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
@@ -120,7 +138,8 @@
         private Button btnRun;
         private Button button2;
         private PictureBox pictureBox1;
-        private TextBox textBox2;
+        private TextBox textBoxCmdLine;
         private PictureBox pictureBox2;
+        private Button button1;
     }
 }
